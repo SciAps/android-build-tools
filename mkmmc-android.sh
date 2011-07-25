9660634b70eca80faf0bbb9bb0d05472e379bbf0
John Young <john.young@logicpd.com>
2011-07-25 11:48:40 -0500

    add some simple checks to hopefully reduce user errors
    -display more information about disk being overwritten
    -exit script if error
    -add MAX_ALLOWED_DISK_SIZE - defaulted to size of Demo Image SD card
     should help prevent overwriting hard drive since most are larger than 4GB
    -default to current directory for source files
    -move overwrite confirm to right before overwriting drive
    -only attempt to copy files if they exist
    -Write first 512 bytes of new partition. see fdisk(8)

--- mkmmc-android.sh
+++ mkmmc-android.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+set -e
+OUT=/dev/null
+#OUT=/dev/stdout
+#set -x
 
+MAX_ALLOWED_DISK_SIZE=4095737856
 EXPECTED_ARGS=1
 if [ $# == $EXPECTED_ARGS ]
 then
 	echo "Assuming Default Locations for Prebuilt Images"
-	$0 $1 Boot_Images/MLO Boot_Images/u-boot.bin Boot_Images/uImage Boot_Images/boot.scr Filesystem/rootfs* Media_Clips
+	$0 $1 ./MLO ./u-boot.bin ./uImage ./boot.scr ./rootfs.tar.bz2 ./Media_Clips ./START_HERE
 	exit
 fi
 
 if [[ -z $1 || -z $2 || -z $3 || -z $4 ]]
 then
 	echo "mkmmc-android Usage:"
-	echo "	mkmmc-android <device> <MLO> <u-boot.bin> <uImage> <boot.scr> <rootfs tar.bz2 > <Optional Media_Clips>"
-	echo "	Example: mkmmc-android /dev/sdc MLO u-boot.bin uImage boot.scr rootfs.tar.bz2 Media_Clips"
+	echo "	mkmmc-android <device>"
+	echo "	  Uses default locations and names of source files/directories"
+	echo "	mkmmc-android <device> <MLO> <u-boot.bin> <uImage> <boot.scr> <rootfs tar.bz2 > <Optional Media_Clips> <Optional START_HERE folder>"
+	echo "	Example: mkmmc-android /dev/sdc MLO u-boot.bin uImage boot.scr rootfs.tar.bz2 Media_Clips START_HERE"
 	exit
 fi
+
+DRIVE=$1
+DEVICE=`basename $DRIVE`
 
 if ! [[ -e $2 ]]
 then
@@ -46,61 +56,127 @@ then
 	exit
 fi
 
-echo "All data on "$1" now will be destroyed! Continue? [y/n]"
+if ! [ -e $DRIVE ]
+then
+	echo "Error: $DRIVE not found!"
+	exit
+fi
+
+# Simplistic sanity check to prevent selecting a larger device
+# such as a secondary hard drive, or attached backup drive.
+DISKSIZE=`sudo fdisk -l $DRIVE | grep Disk | grep $DRIVE`
+SIZE=`echo $DISKSIZE | awk '{print $5}'`
+if [ $SIZE -gt $MAX_ALLOWED_DISK_SIZE ]
+then
+	echo ""
+	echo "*** Warning! Device reports > MAX_ALLOWED_DISK_SIZE ($MAX_ALLOWED_DISK_SIZE). ***"
+	echo "  $DISKSIZE"
+	echo "Are you sure you selected the correct device? [y/n]"
+	read ans
+	if ! [[ $ans == 'y' ]]
+	then
+		exit
+	fi
+fi
+
+for file in $(find /sys/block/$DEVICE/device/ /sys/block/$DEVICE/ -maxdepth 1 2>/dev/null \
+ |egrep '(vendor|model|manfid|name|/size|/sys/block/[msh][mdr]./$|/sys/block/mmcblk./$)'|sort);
+do [ -d $file ] && echo -e "\n  -- DEVICE $(basename $file) --" && continue;
+grep -H . $file|sed -e 's|^/sys/block/||;s|/d*e*v*i*c*e*/*\(.*\):| \1 |'|awk '{if($2 == "size") {printf "%-3s %-6s: %d MB\n", $1,$2,(($3 * 512)/1048576)} else {printf "%-3s %-6s: ", $1,$2;for(i=3;i<NF;++i) printf "%s ",$i;print $(NF) };}';
+done
+echo "";
+
+echo "[Unmounting all existing partitions on the device ]"
+
+devices=`ls /sys/block/$DEVICE/$DEVICE* -d | sed "s^/sys/block/$DEVICE/^^"`
+for f in $devices; do
+	MOUNTCHECK=`mount | grep "^/dev/$f" | wc -l`
+	if [[ $MOUNTCHECK = '1' ]]
+	then
+		MOUNTINFO=`mount | grep "^/dev/$f" | awk '{ print $1 " " $2 " " $3 " " $4 " " $5 }' `
+		echo "  unmounting $f ($MOUNTINFO)"
+		sudo umount /dev/$f
+	fi
+done
+#sudo umount $DRIVE
+
+echo ""
+echo "All data on $DRIVE now will be destroyed! Continue? [y/n]"
 read ans
 if ! [ $ans == 'y' ]
 then
 	exit
 fi
 
-echo "[Unmounting all existing partitions on the device ]"
+echo "[Partitioning $DRIVE...]"
 
-umount $1*
-
-echo "[Partitioning $1...]"
-
-DRIVE=$1
-dd if=/dev/zero of=$DRIVE bs=1024 count=1024 &>/dev/null
-	 
-SIZE=`fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
+SIZE=`sudo fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
 	 
 echo DISK SIZE - $SIZE bytes
  
 CYLINDERS=`echo $SIZE/255/63/512 | bc`
  
 echo CYLINDERS - $CYLINDERS
+
+sudo dd if=/dev/zero of=$DRIVE bs=1024 count=1024 &> $OUT
+
 {
 echo ,9,0x0C,*
 echo ,$(expr $CYLINDERS / 2),,-
 echo ,,0x0C,-
-} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE &> /dev/null
+} | sudo sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE &> $OUT
+
+sudo partprobe $DRIVE
+
+sudo dd if=/dev/zero of="${DRIVE}${PART}1" bs=512 count=1 &> $OUT
+sudo dd if=/dev/zero of="${DRIVE}${PART}2" bs=512 count=1 &> $OUT
+sudo dd if=/dev/zero of="${DRIVE}${PART}3" bs=512 count=1 &> $OUT
 
 echo "[Making filesystems...]"
 
-mkfs.vfat -F 32 -n boot "$1"1 &> /dev/null
-mkfs.ext3 -L rootfs "$1"2 &> /dev/null
-mkfs.vfat -F 32 -n data "$1"3 &> /dev/null
+if [ $DRIVE == '/dev/mmcblk0' ]
+then
+	PART='p'
+else
+	PART=''
+fi
+
+sudo mkfs.vfat -F 32 -n boot "${DRIVE}${PART}1" &> $OUT
+sudo mkfs.ext3 -L rootfs "${DRIVE}${PART}2" &> $OUT
+sudo mkfs.vfat -F 32 -n data "${DRIVE}${PART}3" &> $OUT
 
 echo "[Copying files...]"
 
-mount "$1"1 /mnt
-cp $2 /mnt/MLO
-cp $3 /mnt/u-boot.bin
-cp $4 /mnt/uImage
-cp $5 /mnt/boot.scr
-umount "$1"1
+sudo mount "${DRIVE}${PART}1" /mnt
+sudo cp $2 /mnt/MLO
+sync
+sudo cp $3 /mnt/u-boot.bin
+sync
+sudo cp $4 /mnt/uImage
+sudo cp $5 /mnt/boot.scr
+if [ "$8" ] && [ -d $8 ]
+then
+        echo "[Copying START_HERE directory to boot partition]"
+        sudo cp -r $8 /mnt/START_HERE
+fi
 
-mount "$1"2 /mnt
-tar jxvf $6 -C /mnt &> /dev/null
-chmod 755 /mnt
-umount "$1"2
+sudo umount "${DRIVE}${PART}1"
 
-if [ "$7" ]
+sudo mount "${DRIVE}${PART}2" /mnt
+sudo tar jxvf $6 -C /mnt &> $OUT
+#sudo chmod 755 /mnt
+sudo umount "${DRIVE}${PART}2"
+
+if [ "$7" ] && [ -d $7 ]
 then
 	echo "[Copying all clips to data partition]"
-	mount "$1"3 /mnt
-	cp -r $7/* /mnt/
-	umount "$1"3
+	sudo mount "${DRIVE}${PART}3" /mnt
+	if [ "$(ls -A $7/*)" ]; then
+		sudo cp -r $7/* /mnt/
+	else
+		echo "no media clips to copy"
+	fi
+	sudo umount "${DRIVE}${PART}3"
 fi
 
 echo "[Done]"
