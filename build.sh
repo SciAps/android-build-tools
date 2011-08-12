204da6984bd62ae7084f5a4eba1e20e04b2fc940
Ashwin Bihari <ashwinb@logicpd.com>
2011-08-12 09:14:37 -0400

    - Quiet the mkimage output

--- build.sh
+++ build.sh
@@ -709,7 +709,7 @@ if [ "$DEPLOY" != "NONE" ]; then
     echo
     # Create the boot script for sdcard
     if [ ! -e $BUILDOUTPUT/boot_sd.scr ]; then
-        mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Logic PD Android SD Boot" -d device/logicpd/dm3730logic/boot_sd.cmd out/target/product/dm3730logic/boot_sd.scr
+        mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Logic PD Android SD Boot" -d device/logicpd/dm3730logic/boot_sd.cmd out/target/product/dm3730logic/boot_sd.scr > /dev/null 2>&1
         if [ -e out/target/product/dm3730logic/boot_sd.scr ]; then
             cp out/target/product/dm3730logic/boot_sd.scr $BUILDOUTPUT/boot_sd.scr
         fi
